276224e81c18b534181503bbf105587a8fd4c73f
axatova <147373902+axatova@users.noreply.github.com>
2023-11-14 20:45:06 +0300

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,28 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+    
+        if (S == 'M') {
+        if (height < 1.70) {
+            cout << "Short" << endl;
+        } else if (height >= 1.70 && height < 1.85) {
+            cout << "Normal" << endl;
+        } else {
+            cout << "Tall" << endl;
+        }
+    } else if (S == 'F') {
+        if (height < 1.60) {
+            cout << "Short" << endl;
+        } else if (height >= 1.60 && height < 1.75) {
+            cout << "Normal" << endl;
+        } else {
+            cout << "Tall" << endl;
+        }
+    } else {
+        cout << "Invalid input for sex." << endl;
+    }
 
-    // use return to return your result
-    // make use of control flow statements
+    return 0;
 }
+
+    
