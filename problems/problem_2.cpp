abe894ea81c685e61351603f4ed3c3b8a1f7b8f7
axatova <147373902+axatova@users.noreply.github.com>
2023-11-14 20:48:47 +0300

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,17 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+    cin>>i>>j>>k;
+    if(i>j && i>k) {
+        cout << " The maximum number is " << i << endl;
+    }else if( j>i && j>k) {
+        cout << "The maximum number is " << j << endl;
+    }else{
+        cout<<" The maximum number is "<< k<< endl;
+    }
+
+    return 0;
+}
 
     return result;
 }
